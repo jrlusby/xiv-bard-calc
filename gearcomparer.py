ac3fe5aa876baef1c43bd256dc3f70e76db1efe2
jlusby <jlusby42@gmail.com>
2015-03-12 19:24:22 -0700

    wip

--- gearcomparer.py
+++ gearcomparer.py
@@ -13,32 +13,39 @@ def sumdps(STR, CRIT, DTR, SS, WD, weapon_delay):
 
     critrate = (CRIT*0.0697-18.437)/100
     critrate = critrate+.1 # straight shot
+    critrate = critrate+(.1*1.0/4)
     critmodifier = 1 + 0.5*critrate
     potency = bardrotation(critrate, SS)
     auto = autoattackdamage(WD, STR, DTR, weapon_delay)/weapon_delay*cdmodifier*critmodifier
     ability = abilitydamage(WD, STR, DTR, potency)
-    # print "pot, auto, ability", potency, auto, ability
-    noir = (auto+ability)
+    noir = (auto+ability)*(1+.1*1.0/4.0)
+    print "DEX, pot, auto, ability", STR, potency, auto, ability, noir
 
-    ircritrate = critrate+.1
-    ircritmodifier = 1 + 0.5*ircritrate
-    irpotency = bardrotation(ircritrate, SS)
-    irauto = autoattackdamage(WD, STR, DTR, weapon_delay)/weapon_delay*cdmodifier*ircritmodifier
-    irability = abilitydamage(WD, STR, DTR, irpotency)
-    ir = (irauto+irability)
+    # ircritrate = critrate+.1
+    # ircritmodifier = 1 + 0.5*ircritrate
+    # irpotency = bardrotation(ircritrate, SS)
+    # irauto = autoattackdamage(WD, STR, DTR, weapon_delay)/weapon_delay*cdmodifier*ircritmodifier
+    # irability = abilitydamage(WD, STR, DTR, irpotency)
+    # ir = (irauto+irability)
 
-    return ir*15/60 + noir*45/60
-    # return noir
+    # return ir*15/60 + noir*45/60
+    return noir
 
 # expects weapon as a 2 element list of format [WeaponDamage, Delay]
 def calc_weights(STR, ACC, CRIT, DTR, SKS, WEP):
     SKS = SKS - 341
+    # partybuff = 1.03
+    partybuff = 1.0
+    hawkeseye = 1 + (.15*20/90)
+    # STR = math.floor(STR*partybuff*hawkeseye)
+    STR = STR*partybuff*hawkeseye
     base = sumdps(STR, CRIT, DTR, SKS, WEP[0], WEP[1])
     strinc = sumdps(STR+5, CRIT, DTR, SKS, WEP[0], WEP[1])-base
-    critinc = sumdps(STR, CRIT+5, DTR, SKS, WEP[0], WEP[1])-base
     detinc = sumdps(STR, CRIT, DTR+5, SKS, WEP[0], WEP[1])-base
-    ssinc = sumdps(STR, CRIT, DTR, SKS+5, WEP[0], WEP[1])-base
+    critinc = sumdps(STR, CRIT+5, DTR, SKS, WEP[0], WEP[1])-base
     wdinc = sumdps(STR, CRIT, DTR, SKS, WEP[0]+5, WEP[1])-base
+    ssinc = sumdps(STR, CRIT, DTR, SKS+5, WEP[0], WEP[1])-base
+    print strinc, detinc, critinc, wdinc, ssinc
     return [strinc/strinc, 0, critinc/strinc, detinc/strinc, ssinc/strinc, wdinc/strinc]
 
 # calculates weights from current set, gives fairly useless numbers should be ignored
@@ -135,7 +142,7 @@ def main():
     highallaganbow = [48, 3.36]
     zetabow = [52, 3.04]
 
-    weights = calc_weights(664, 536, 520, 338, 394, zetabow)
+    weights = calc_weights(664, 536, 520, 338, 389, zetabow)
     # weights = calc_dps(664, 536, 520, 338, 394, zetabow)
     print weights
     # weights2 = calc_weights(664, 536, 710, 338, 389, zetabow)
